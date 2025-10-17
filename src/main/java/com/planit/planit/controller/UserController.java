43dda20c019c72f20bf03fc59f89e68c0f3d38b8
moon <gkdlrne97@naver.com>
2025-10-17 16:13:53 +0900

    swagger적용 및 테스트케이스 추가

--- src/main/java/com/planit/planit/controller/UserController.java
+++ src/main/java/com/planit/planit/controller/UserController.java
@@ -14,15 +14,30 @@ import org.springframework.web.bind.annotation.RestController;
 import com.planit.planit.model.dto.UserDTO;
 import com.planit.planit.model.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/test")
+/////////////////TODO 5. Controller  설명 설정
+@Tag(name = "테스트용", description = "테스트용 유저 확인")
 public class UserController {
     private final UserService userService;
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+	/////////////////TODO 6. Controller 함수 설명 설정
+	@Operation(	summary = "유저 전체 목록 확인", 
+			description = "테스트용 유저 전체 목록 확인",
+			responses ={
+				@ApiResponse(responseCode = "200", description = "목록 성공"),
+				@ApiResponse(responseCode = "404", description = "전체목록확인에러!!"),
+				@ApiResponse(responseCode = "500", description = "서버에러!!")
+			}
+	)
     @GetMapping
     public List<UserDTO> getAll() {
         return userService.getAllUsers();
