f3cac35cd5c1f133ba3c36b3e50b7c551408a3ff
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-10-21 14:03:55 +0900

    chore: 에러메시지 수정

--- src/main/java/com/planit/planit/global/common/exception/ErrorCode.java
+++ src/main/java/com/planit/planit/global/common/exception/ErrorCode.java
@@ -9,12 +9,12 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // 4XX
-    METHOD_ARGUMENT_NOT_VALID(HttpStatus.BAD_REQUEST, "잘못된 [인자]입니다."),
-    PARAMETER_NOT_FOUND(HttpStatus.BAD_REQUEST, "요청에 [Parameter]가 존재하지 않습니다."),
+    METHOD_ARGUMENT_NOT_VALID(HttpStatus.BAD_REQUEST, "잘못된 인자입니다."),
+    PARAMETER_NOT_FOUND(HttpStatus.BAD_REQUEST, "요청에 Parameter가 존재하지 않습니다."),
     PATH_VARIABLE_NOT_FOUND(HttpStatus.BAD_REQUEST, "경로 변수가 누락되었습니다."),
     TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "요청 파라미터의 타입이 올바르지 않습니다."),
     INVALID_REQUEST_BODY(HttpStatus.BAD_REQUEST, "요청 본문을 읽을 수 없습니다."),
-    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 [RESOURCE, URL]를 찾을 수 없습니다."),
+    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 RESOURCE, URL를 찾을 수 없습니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 HTTP 메서드입니다."),
@@ -23,7 +23,7 @@ public enum ErrorCode {
     CONFLICT(HttpStatus.CONFLICT, "충돌이 발생했습니다."),
 
     // 5XX
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "[Server] 내부 에러가 발생했습니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Server 내부 에러가 발생했습니다.");
 
     private final HttpStatus status;
 
