49ca4b0dc2c610a96b14155a59bf762fa9ea90e2
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-10-22 20:53:09 +0900

    feat: usermapper 인터페이스 추가

--- src/main/java/com/planit/planit/domain/user/mapper/UserMapper.java
+++ src/main/java/com/planit/planit/domain/user/mapper/UserMapper.java
@@ -1,18 +1,15 @@
 package com.planit.planit.domain.user.mapper;
 
-import java.util.List;
-
+import com.planit.planit.domain.user.model.UserAccount;
+import com.planit.planit.domain.user.model.UserLevel;
 import org.apache.ibatis.annotations.Mapper;
-
-import com.planit.planit.domain.user.dto.UserDTO;
-
-
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapper {
-    List<UserDTO> findAll();
-    UserDTO findById(Long id);
-    void insert(UserDTO user);
-    void update(UserDTO user);
-    void delete(Long id);
+	UserAccount findByUid(@Param("uid") String uid);
+	void insertUser(UserAccount user);
+	int updateLastLogin(@Param("uid") String uid);
+	int updateUserLevel(@Param("uid") String uid,
+		@Param("userLevel") UserLevel userLevel);
 }
