8c304c29f21dfda09a479b87024c9724dd4c2b72
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-10-22 20:51:33 +0900

    feat: 로그인용 임시 컨트롤러 구í현

---
+++ src/main/java/com/planit/planit/domain/user/contoller/AuthController.java
@@ -0,0 +1,26 @@
+package com.planit.planit.domain.user.contoller;
+
+import org.springframework.security.core.Authentication;
+import com.planit.planit.domain.user.mapper.UserMapper;
+import com.planit.planit.domain.user.model.UserAccount;
+import com.planit.planit.global.common.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+	private final UserMapper userMapper;
+
+	@GetMapping("/me")
+	public ApiResponse<UserAccount> me(Authentication auth) {
+		String uid = auth.getName();
+		var user = userMapper.findByUid(uid);
+		return ApiResponse.success("내 정보", user);
+	}
+}
+
