35ccac38c139c45456bb1f7d4a32dcd9ed9673bd
moon <gkdlrne97@naver.com>
2025-10-23 14:14:32 +0900

    hotfix: swagger 보안이슈수정

--- src/main/java/com/planit/planit/global/config/SecurityConfiguration.java
+++ src/main/java/com/planit/planit/global/config/SecurityConfiguration.java
@@ -1,14 +1,9 @@
 package com.planit.planit.global.config;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.planit.planit.domain.user.service.FirebaseAccountService;
-import com.planit.planit.global.security.AuthenticationFilter;
-import com.planit.planit.global.security.Json401EntryPoint;
-import com.planit.planit.global.security.Json403AccessDeniedHandler;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -17,39 +12,39 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.planit.planit.domain.user.service.FirebaseAccountService;
+import com.planit.planit.global.security.AuthenticationFilter;
+import com.planit.planit.global.security.Json401EntryPoint;
+import com.planit.planit.global.security.Json403AccessDeniedHandler;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
-	private final ObjectProvider<FirebaseAccountService> accountServiceProvider;
-	private final FirebaseAuth firebaseAuth;
+  private final ObjectProvider<FirebaseAccountService> accountServiceProvider;
+  private final FirebaseAuth firebaseAuth;
 
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		var authenticationFilter = new AuthenticationFilter(accountServiceProvider, firebaseAuth);
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    var authenticationFilter = new AuthenticationFilter(accountServiceProvider, firebaseAuth);
 
-		http
-			.csrf(AbstractHttpConfigurer::disable)
-			.cors(Customizer.withDefaults())
-			.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.formLogin(AbstractHttpConfigurer::disable)
-			.httpBasic(AbstractHttpConfigurer::disable)
-			.authorizeHttpRequests(auth -> auth
-				.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-				.requestMatchers("/", "/index.html", "/favicon.ico").permitAll()
-				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-				.requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
-				.requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll() // 회원가입 허용 용도, 추후 변경 가능
-				.anyRequest().authenticated()
-			)
-			.exceptionHandling(ex -> ex
-				.authenticationEntryPoint(new Json401EntryPoint())
-				.accessDeniedHandler(new Json403AccessDeniedHandler())
-			)
-			.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    http.csrf(AbstractHttpConfigurer::disable).cors(Customizer.withDefaults())
+        .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .formLogin(AbstractHttpConfigurer::disable).httpBasic(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+            .requestMatchers("/", "/index.html", "/favicon.ico").permitAll()
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+            .requestMatchers("/api/v1/**").permitAll() // api/v1 하위 모든메소드, 도메인 허용 (추후 변경 해야됨)
+            .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll() // 회원가입 허용 용도, 추후 변경 가능
+            .anyRequest().authenticated())
+        .exceptionHandling(ex -> ex.authenticationEntryPoint(new Json401EntryPoint())
+            .accessDeniedHandler(new Json403AccessDeniedHandler()))
+        .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-		return http.build();
-	}
+    return http.build();
+  }
 }
