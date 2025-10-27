b81f5bf2d8198ff618e9366e2fc1da7d4b555639
Jiyeon <xhdrp@sookmyung.ac.kr>
2025-10-27 13:40:44 +0900

    feat: 스케쥴링 설정 추가

---
+++ src/main/java/com/planit/planit/global/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.planit.planit.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
