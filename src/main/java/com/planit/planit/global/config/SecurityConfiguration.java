0f851171d6ec4738e7ca0f2e3a7fe2a3956c9d98
tree0-0 <gkdlrne97@naver.com>
2025-12-18 16:12:49 +0900

    [FIX] 로그아웃 api 인증해체 (#63)
    * feat: enroll api 구현
    
    * feat: 경쟁 조건 보완
    
    * feat: token request 분리
    
    * fix: 예외처리 보강
    
    * fix: mybootcamp 수정
    
    * Delete src/main/resources/application.properties
    
    * fix: properties 복원
    
    * feat: 로그인, 로그아웃 구현
    
    * fix: 수정
    
    * feat: 보ì쿠키 강화
    
    * fix: properties
    
    * fix: ã login, 내 정보 응답 수정
    
    * chore: 리턴 타입 수정 및 스웨거 수정
    
    * refactor: auth 관리 분리
    
    * fix: 보안 ì필터 수정
    
    * chore: 오타 수정
    
    * fix: 로그아웃 apapi 인증 해체
    
    ---------
    
    Co-authored-by: Chaejy <xhdrp@sookmyung.ac.kr>
    Co-authored-by: Jiyeon <144954836+Chaejy@users.noreply.github.com>

--- src/main/java/com/planit/planit/global/config/SecurityConfiguration.java
+++ src/main/java/com/planit/planit/global/config/SecurityConfiguration.java
@@ -13,76 +13,60 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import com.google.firebase.auth.FirebaseAuth;
 import com.planit.planit.domain.user.service.FirebaseAccountService;
 import com.planit.planit.global.security.AuthenticationFilter;
 import com.planit.planit.global.security.Json401EntryPoint;
 import com.planit.planit.global.security.Json403AccessDeniedHandler;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
-	private final ObjectProvider<FirebaseAccountService> accountServiceProvider;
-	private final FirebaseAuth firebaseAuth;
+  private final ObjectProvider<FirebaseAccountService> accountServiceProvider;
+  private final FirebaseAuth firebaseAuth;
 
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of(
-			"http://localhost:3000",
-			"https://planit-tau-seven.vercel.app"
-		));
-		config.setAllowedMethods(List.of(
-			"GET","POST","PUT","PATCH","DELETE","OPTIONS"
-		));
-		config.setAllowedHeaders(List.of("*"));
-		config.setAllowCredentials(true);
-		config.setMaxAge(3600L);
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration config = new CorsConfiguration();
+    config
+        .setAllowedOrigins(List.of("http://localhost:3000", "https://planit-tau-seven.vercel.app"));
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowCredentials(true);
+    config.setMaxAge(3600L);
 
-		UrlBasedCorsConfigurationSource source =
-			new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", config);
-		return source;
-	}
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+    return source;
+  }
 
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		var authenticationFilter = new AuthenticationFilter(accountServiceProvider, firebaseAuth);
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    var authenticationFilter = new AuthenticationFilter(accountServiceProvider, firebaseAuth);
 
-		http
-			.csrf(AbstractHttpConfigurer::disable)
-			.cors(Customizer.withDefaults())
-			.headers(headers -> headers
-				.addHeaderWriter((request, response) -> {
-					response.setHeader(
-						"Cross-Origin-Opener-Policy",
-						"unsafe-none"
-					);
-				})
-			)
-			.sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-			.formLogin(AbstractHttpConfigurer::disable)
-			.httpBasic(AbstractHttpConfigurer::disable)
-			.authorizeHttpRequests(auth -> auth
-				.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-				.requestMatchers("/", "/index.html", "/favicon.ico").permitAll()
-				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-				.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-				.requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
-				.requestMatchers("/api/**").authenticated() // API 요청은 인증 처리
-				.anyRequest().permitAll() // 나머지 요청 허용
-			)
-			.exceptionHandling(ex -> ex
-				.authenticationEntryPoint(new Json401EntryPoint())
-				.accessDeniedHandler(new Json403AccessDeniedHandler())
-			)
-			.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
-		return http.build();
-	}
+    http.csrf(AbstractHttpConfigurer::disable).cors(Customizer.withDefaults())
+        .headers(headers -> headers.addHeaderWriter((request, response) -> {
+          response.setHeader("Cross-Origin-Opener-Policy", "unsafe-none");
+        })).sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .formLogin(AbstractHttpConfigurer::disable).httpBasic(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+            .requestMatchers("/", "/index.html", "/favicon.ico").permitAll()
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
+            .requestMatchers("/api/v1/auth/logout").permitAll()// logout api는 인증 안해도 가능하게
+            .requestMatchers("/api/**").authenticated() // API 요청은 인증 처리
+            .anyRequest().permitAll() // 나머지 요청 허용
+        )
+        .exceptionHandling(ex -> ex.authenticationEntryPoint(new Json401EntryPoint())
+            .accessDeniedHandler(new Json403AccessDeniedHandler()))
+        .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
+  }
 }
